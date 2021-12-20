f245b0da623f398d8b46ccefa760469edd4ffef0
andrew bostick <andbostick@gmail.com>
2021-12-19 20:46:21 -0500

    logo size change removed typo from hero

--- components/Hero.js
+++ components/Hero.js
@@ -1,13 +1,16 @@
 import Image from "next/image";
 
 export default function Hero({ src, height, width }) {
-  return <div><Image src={src} height={height} width={width} />;
-    <style jsx>
-      {`
-      div{
-        margin-bottom: 40px;
-      }
-      `}
-    </style>
-  </div>
+  return (
+    <div>
+      <Image src={src} height={height} width={width} />
+      <style jsx>
+        {`
+          div {
+            margin-bottom: 30px;
+          }
+        `}
+      </style>
+    </div>
+  );
 }
--- components/Nav.js
+++ components/Nav.js
@@ -1,21 +1,21 @@
 import Image from "next/image";
 
 const logo =
-  "https://images.ctfassets.net/pk5fqs6e1wc2/DdYzZeblPgkrt6LMEXcNw/f72a461d8d63dd16ff39fe028d7e60a2/nuclearLogo.png?w=200&h=200&fit=pad";
+  "https://images.ctfassets.net/pk5fqs6e1wc2/DdYzZeblPgkrt6LMEXcNw/f72a461d8d63dd16ff39fe028d7e60a2/nuclearLogo.png?w=150&h=150&fit=pad";
 export default function Navbar() {
   return (
     <div className="logo">
-      <Image className="img" src={logo} height="200px" width="200px" />
+      <Image className="img" src={logo} height="150px" width="150px" />
       <style jsx>
         {`
           .logo {
-            width: 200px;
-            height: 200px;
+            width: 150px;
+            height: 150px;
             border-radius: 50%;
             position: absolute;
             overflow: hidden;
-            top:12%;
-            left:23%;
+            top:14.5%;
+            left:30%;
           }
           .logo .img {
             min-width: 100%;
